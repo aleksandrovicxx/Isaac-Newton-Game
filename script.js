cdff81f7e1ceb8686f4a0d20e4331f4c92dec1f3
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-03-21 10:36:22 +0100

    some wrong with script

--- script.js
+++ script.js
@@ -24,4 +24,31 @@ document.addEventListener("DOMContentLoaded", () => {
         isaac.style.left = newPosition + 'px'
     }
 
+    //creating apples   
+    let creatingApple = () => {
+        const apple = document.createElement('div')
+        apple.className.add('apple')
+        const randomX = Math.random() * (gameContainerWidth - 40)
+        apple.style.left = randomX + 'px'
+        gameContainer.appendChild(apple)
+
+        //falling apples
+        const fallInterval = setInterval(function () {
+            const isaacArea = isaac.getBoundingClientRect()
+            const applesArea = apple.getBoundingClientRect()
+
+            if(applesArea.bottom >= isaacArea.top && applesArea.right >= isaacArea.left && isaacArea.right >= applesArea.left) {
+                apple.remove()
+                clearInterval(fallInterval)
+            } else if (applesArea.bottom >= gameContainer.offsetHeight) {
+                apple.remove()
+                clearInterval(fallInterval)
+            } else {
+                apple.style.top = apple.offsetTop + 5 + 'px' //moving do down
+            }
+        }, 20)
+    }
+
+    setInterval(creatingApple, 2000)
+
 })
